6a609b32e09fd189847cc16e1b4ffb92f3e9a147
SmirnovRoman94 <smirnow3217831@gmail.com>
2022-01-04 12:54:35 +0300

    add project

---
+++ components/bar.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-toolbar dark color="blue">
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-toolbar-title class="white--text">
+      <nuxt-link class="title" to="/">iTunes Search</nuxt-link>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn to="/" icon>
+      <v-icon>refresh</v-icon>
+    </v-btn>
+  </v-toolbar>
+</template>
+<script>
+</script>
+<style scoped>
+.title {
+  text-decoration: none !important;
+}
+.title:visited{
+  color: white;
+}
+</style>
